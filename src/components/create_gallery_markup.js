64604668271fdf050b21115b865bbc91f82b792a
Marta Żak <martysik19@gmail.com>
2022-05-13 19:11:51 +0200

    added css styles

--- src/components/create_gallery_markup.js
+++ src/components/create_gallery_markup.js
@@ -1,3 +1,3 @@
 export default function createGalleryMarkup(image) {
-  return `<div class="photo-card gallery"><a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /><a/><div class="info"><p class="info-item"><b>Likes </b>${image.likes}</p><p class="info-item"><b>Views </b>${image.views}</p><p class="info-item"><b>Comments </b>${image.comments}</p><p class="info-item"><b>Downloads </b>${image.downloads}</p></div></div>`;
+  return `<div class="photo-card gallery"><a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width= 300px height=200px /></a><div class="info"><p class="info-item"><b>Likes </b>${image.likes}</p><p class="info-item"><b>Views </b>${image.views}</p><p class="info-item"><b>Comments </b>${image.comments}</p><p class="info-item"><b>Downloads </b>${image.downloads}</p></div></div>`;
 }
